2c686125c2789ca30eb558d93c44b794101f4103
sgrillon <sgrillon@hotmail.fr>
2017-03-02 22:27:11 +0100

    add travis script

---
+++ test/run.sh
@@ -0,0 +1,14 @@
+cd $(dirname $0)
+cd 
+mvn clean compile
+# java -jar target/flags-rest-service-0.1.1.jar &
+# PID=$!
+# sleep 15
+
+# kill -9 $PID
+
+echo "NORA-UI is ready"
+
+rm -rf target
+
+exit
