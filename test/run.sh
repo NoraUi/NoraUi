dd9c37bc9c0935436c5854816f60355fb2789bea
sgrillon <sgrillon@hotmail.fr>
2017-03-16 15:22:10 +0100

    fixed issues on Maven command

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 cd $(dirname $0)
 cd ..
-clean clean deploy javadoc:javadoc -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
+mvn clean test javadoc:javadoc -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
 # java -jar target/flags-rest-service-0.1.1.jar &
 # PID=$!
 # sleep 15
