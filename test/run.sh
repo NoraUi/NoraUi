03c1cfc424ded7142708ba0ffbda506083a0c4c4
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-29 18:26:43 +0200

    test travis api

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@ cd $(dirname $0)
 cd ..
 mvn clean test javadoc:javadoc -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
 
-travis raw /
+$ travis logs
 
 echo "NORA-UI is ready"
 
