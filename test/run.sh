7fa2285081e0ede7d51189bef111b06a7959aea5
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 22:16:18 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -2,8 +2,18 @@ cd $(dirname $0)
 cd ..
 mvn clean test javadoc:javadoc -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
 
-echo "NORA-UI is ready"
+curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
 
-rm -rf target
+expectation=`sed -n 's:.*<EXPECTED_RESULTS>\(.*\)</EXPECTED_RESULTS>.*:\1:p' nonaui.log | head -n 1`
+nb_expectation=`sed -n ":;s/$expectation//p;t" nonaui.log | sed -n '$='`
+echo "******** Check if all counter is OK ********"
+if [ "$nb_expectation" == "2" ]; then
+    echo SUCCESS
+    let ret=0
+else
+    echo FAIL
+    let ret=255
+    exit $ret
+fi
 
 exit
