046149acaa939bcf793d9cd53bb3651af8947c5b
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 14:45:47 +0200

    Update ci-validation.sh

--- test/ci-validation.sh
+++ test/ci-validation.sh
@@ -1,4 +1,5 @@
 echo "******** start log *****"
+echo `pwd`
 travis logs --help
 echo "y" | travis login --github-token "6c33d8f5d9a9f2fbe2bb1a7d1ec243ac9a1aebb0"
 varrr = $(`travis logs --no-interactive`)
