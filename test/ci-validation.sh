ab8bc82c72554d1baae1dfe6882505dffabd133f
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 22:09:51 +0200

    Update ci-validation.sh

--- test/ci-validation.sh
+++ test/ci-validation.sh
@@ -1,13 +1,9 @@
-echo "******** start log *****"
 curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
-cat nonaui.log
-
-echo "******** count log *****"
 
 expectation=`sed -n 's:.*<EXPECTED_RESULTS>\(.*\)</EXPECTED_RESULTS>.*:\1:p' nonaui.log | head -n 1`
 nb_expectation=`sed -n ":;s/$expectation//p;t" nonaui.log | sed -n '$='`
-echo "Check if all counter is OK"
-if [ "$nb_expectation" == "3" ]; then
+echo "******** Check if all counter is OK ********"
+if [ "$nb_expectation" == "2" ]; then
     echo SUCCESS
     let ret=0
 else
@@ -16,4 +12,4 @@ else
     exit $ret
 fi
 
-echo "******** stop log *****"
+exit
