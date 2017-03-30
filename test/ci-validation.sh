42ab8f7df865363c5742bbb13a97582343b49307
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 22:04:21 +0200

    Update ci-validation.sh

--- test/ci-validation.sh
+++ test/ci-validation.sh
@@ -5,10 +5,15 @@ cat nonaui.log
 echo "******** count log *****"
 
 expectation=`sed -n 's:.*<EXPECTED_RESULTS>\(.*\)</EXPECTED_RESULTS>.*:\1:p' nonaui.log | head -n 1`
-echo $expectation
-
-sed -n ":;s/$expectation//p;t" nonaui.log | sed -n '$='
-
-
+nb_expectation=`sed -n ":;s/$expectation//p;t" nonaui.log | sed -n '$='`
+echo "Check if all counter is OK"
+if [ "$nb_expectation" == "3" ]; then
+    echo SUCCESS
+    let ret=0
+else
+    echo FAIL
+    let ret=255
+    exit $ret
+fi
 
 echo "******** stop log *****"
