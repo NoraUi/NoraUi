b2c0ce4029306239537040435faf680d9ad9daaa
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 09:30:59 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -2,14 +2,6 @@ cd $(dirname $0)
 cd ..
 mvn clean test javadoc:javadoc -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
 
-ruby -v
-
-gem install travis -v 1.8.8 --no-rdoc --no-ri --skip-completion-check
-
-travis version
-
-travis logs
-
 echo "NORA-UI is ready"
 
 rm -rf target
