52842bbbfb9748d36e429a85ff3949396362573f
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 15:11:32 +0200

    Update ci-validation.sh

--- test/ci-validation.sh
+++ test/ci-validation.sh
@@ -1,8 +1,8 @@
 echo "******** start log *****"
-pwd
-travis logs --help
-echo "y" | travis login --github-token "6c33d8f5d9a9f2fbe2bb1a7d1ec243ac9a1aebb0"
-travis logs 97.1 --no-interactive > toto.txt
-ls -l
-#echo "yes" | travis logs > /dev/null
+curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > toto.txt
+
+cat toto.txt
+
+
+
 echo "******** stop log *****"
