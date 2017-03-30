90dc9f2786886df8d25372ddfc25e27f6849dbf9
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 15:34:50 +0200

    Update ci-validation.sh

--- test/ci-validation.sh
+++ test/ci-validation.sh
@@ -4,7 +4,7 @@ cat nonaui.log
 
 echo "******** count log *****"
 
-sed -n ':;s/INFO//p;t' nonaui.log | sed -n '$='
+sed -n ':;s/~/<EXPECTED_RESULTS>(.*)<\/EXPECTED_RESULTS>///p;t' nonaui.log | sed -n '$='
 
 
 
