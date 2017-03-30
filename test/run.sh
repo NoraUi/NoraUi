3bd93fffe019767bf3dd47f3840e21350b2f3123
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 22:19:59 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -7,7 +7,7 @@ curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > 
 expectation=`sed -n 's:.*<EXPECTED_RESULTS>\(.*\)</EXPECTED_RESULTS>.*:\1:p' nonaui.log | head -n 1`
 nb_expectation=`sed -n ":;s/$expectation//p;t" nonaui.log | sed -n '$='`
 echo "******** Check if all counter is OK ********"
-if [ "$nb_expectation" == "2" ]; then
+if [ "$nb_expectation" == "3" ]; then
     echo SUCCESS
     let ret=0
 else
