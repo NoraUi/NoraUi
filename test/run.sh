ec19e415e77bc46e741367dfdfa8dac3662137ae
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-03-30 15:34:44 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,6 @@
 cd $(dirname $0)
 cd ..
-#mvn clean test javadoc:javadoc -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
-mvn clean compile
+mvn clean test javadoc:javadoc -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
 
 echo "NORA-UI is ready"
 
