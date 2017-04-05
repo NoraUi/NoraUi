1bcf1ccc2826a8fcac3eba13417b5674fb0b729f
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-04-06 00:17:27 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -4,12 +4,12 @@ mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package javadoc:javadoc s
 
 curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
 
-counters=`sed -n 's:.*<EXPECTED_RESULTS>\(.*\)</EXPECTED_RESULTS>.*:\1:p' nonaui.log | head -n 1`
-nb_counters=`sed -n ":;s/$counters//p;t" nonaui.log | sed -n '$='`
+counters=$(sed -n 's:.*<EXPECTED_RESULTS>\(.*\)</EXPECTED_RESULTS>.*:\1:p' nonaui.log | head -n 1)
+nb_counters=$(sed -n ":;s/$counters//p;t" nonaui.log | sed -n '$=')
 
 # check if BUILD FAILURE finded in logs
-nb_failure=`sed -n ":;s/BUILD FAILURE//p;t" nonaui.log | sed -n '$='`
-if [ "$nb_failure" != "0" ]; then
+nb_failure=$(sed -n ":;s/BUILD FAILURE//p;t" nonaui.log | sed -n '$=')
+if [ "$nb_failure" != "" ]; then
     echo "******** BUILD FAILURE find $nb_failure time in build"
     exit 255
 fi
