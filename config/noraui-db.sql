bc2a50c55e06463f9466bb624ca3cafc62639005
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-04-10 21:25:19 +0200

    Update noraui-db.sql

--- config/noraui-db.sql
+++ config/noraui-db.sql
@@ -1,9 +1,9 @@
 CREATE TABLE hello (
-    login text,
-    password text
+    author text,
+    zip text,
+    city text,
+    element text,
+    date text
 );
 
-INSERT INTO hello VALUES ('user', 'user');
-INSERT INTO hello VALUES ('user', 'user');
-INSERT INTO hello VALUES ('user', 'user');
-INSERT INTO hello VALUES ('user', 'user');
+INSERT INTO hello VALUES ('Jenkins T1', '35000', 'Rennes', '-smile', '16/01/2020');
