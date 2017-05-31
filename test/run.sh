ba2a28ca651330734e30eb79487b7de64b24d98a
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-05-31 14:16:00 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -31,6 +31,7 @@ else
 fi
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+     echo "******** Starting gpg"
      #openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
      #gpg --fast-import cd/signingkey.asc
 fi
