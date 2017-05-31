5d0e0f2761eea423daa668edec173aaa1d766242
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-05-31 12:08:00 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -31,6 +31,11 @@ else
 fi
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+     #openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
+     #gpg --fast-import cd/signingkey.asc
+fi
+
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "******** Starting deploy"
     #mvn clean deploy -Pdeploy --settings test/mvnsettings.xml
 fi
