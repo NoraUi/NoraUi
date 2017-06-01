95e1e01f45e9f6fb921024f6663d56edefd041a3
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-06-01 17:43:38 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -35,7 +35,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
      #echo "******** Starting decrypt codesigning.asc.enc"
      #openssl aes-256-cbc -K $encrypted_a4391f72a6ef_key -iv $encrypted_a4391f72a6ef_iv -in test/codesigning.asc.enc -out test/codesigning.asc -d
      echo "******** Starting gpg import"
-     gpg --fast-import test/codesigning.asc --batch
+     gpg2 --fast-import test/codesigning.asc --batch
 fi
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
