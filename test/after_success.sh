17bdf34ad787ee31c918e98cac4123b4e88f498c
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-06-02 16:03:13 +0200

    Update after_success.sh

--- test/after_success.sh
+++ test/after_success.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "******** Starting deploy"
-    mvn clean deploy -Pdeploy --settings test/mvnsettings.xml -DskipTests=true
+    mvn clean deploy -Psnapshot --settings test/mvnsettings.xml -DskipTests=true
     exit $?
 fi
 echo "******** END ********"
