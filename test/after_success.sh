fc3323f0e274671692f7fcc449446a936c3a3c02
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-06-03 22:36:49 +0200

    Update after_success.sh

--- test/after_success.sh
+++ test/after_success.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mvn_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec)
+echo "Maven version is $mvn_version"
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "******** Starting deploy"
     mvn clean deploy -Psnapshot --settings test/mvnsettings.xml -DskipTests=true
     exit $?
 fi
-echo "******** END ********"
