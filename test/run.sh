78b28ac85b3cb8a88c02a057d8676ab8f9473bdf
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-06-22 22:58:19 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -7,13 +7,49 @@ ls -l
 java -jar noraui-datas-webservices-1.0.0-20170622.195832-1.jar &
 PID=$!
 sleep 30
-curl -s --header "Accept: application/json" http://localhost:8084/noraui/api/hello/columns > actual_hello_columns.json
-curl -s --header "Accept: application/xml" http://localhost:8084/noraui/api/hello/columns > actual_hello_columns.xml
-kill -9 $PID
+curl -s --header "Accept: application/json" http://localhost:8084/noraui/api/hello/columns > target/actual_hello_columns.json
+curl -s --header "Accept: application/xml" http://localhost:8084/noraui/api/hello/columns > target/actual_hello_columns.xml
 
+echo "Let's look at the actual results: `cat target/actual_hello_columns.json`"
+echo "And compare it to: `cat ../test/expected_hello_columns.json`"
+if diff -w ../test/expected_hello_columns.json target/actual_hello_columns.json
+    then
+        echo SUCCESS
+        let ret=0
+    else
+        echo FAIL
+        let ret=255
+        exit $ret
+fi
+
+echo "Let's look at the actual results: `cat target/actual_hello_columns.xml`"
+echo "And compare it to: `cat test/expected_hello_columns.xml`"
+if diff -w test/expected_hello_columns.xml target/actual_hello_columns.xml
+    then
+        echo SUCCESS
+        let ret=0
+    else
+        echo FAIL
+        let ret=255
+        exit $ret
+fi
+echo "******** noraui-datas-webservices STARTED"
+
+#
+# clean tmp files
+rm -rf target
+
+#
+#
+# start NoraUi part =>
 cd $(dirname $0)
 cd ..
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package javadoc:javadoc sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.organization=noraui -Dsonar.login=$SONAR_TOKEN -Dcucumber.options="--tags @hello,@bonjour,@blog,@playToLogoGame,@jouerAuJeuDesLogos" -PscenarioInitiator,javadoc,unit-tests -Dmaven.test.failure.ignore=true
+
+#
+# kill Web Services (REST)
+kill -9 $PID
+echo "******** noraui-datas-webservices STOPED"
 
 #
 # read Maven console
