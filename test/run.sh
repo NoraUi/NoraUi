0bcfd4c4a4e437149d0d6f8caeef367911ad17d6
GRILLON Stéphane <sgrillon14@users.noreply.github.com>
2017-07-05 13:03:48 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -2,9 +2,9 @@
 
 #
 # take noraui-datas-webservices from Maven Central and Start Web Services (REST)
-wget -U "Any User Agent" https://oss.sonatype.org/service/local/repositories/snapshots/content/com/github/noraui/noraui-datas-webservices/1.0.0-SNAPSHOT/noraui-datas-webservices-1.0.0-20170622.195832-1.jar
+wget -U "Any User Agent" https://oss.sonatype.org/service/local/repositories/snapshots/content/com/github/noraui/noraui-datas-webservices/1.0.0-SNAPSHOT/noraui-datas-webservices-1.0.0-20170705.103534-25.jar
 
-java -jar noraui-datas-webservices-1.0.0-20170622.195832-1.jar &
+java -jar noraui-datas-webservices-1.0.0-20170705.103534-25.jar &
 PID=$!
 sleep 30
 curl -s --header "Accept: application/json" http://localhost:8084/noraui/api/hello/columns > actual_hello_columns.json
