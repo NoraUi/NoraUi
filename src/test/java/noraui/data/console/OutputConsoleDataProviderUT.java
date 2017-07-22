a33972f02c472059eb5e4343a1eb8a2b59e84ffd
sgrillon14 <sgrillon@hotmail.fr>
2017-07-23 01:37:10 +0200

    fixed issue (consoleDataProvider is a log, not a println)

--- src/test/java/noraui/data/console/OutputConsoleDataProviderUT.java
+++ src/test/java/noraui/data/console/OutputConsoleDataProviderUT.java
@@ -1,31 +1,21 @@
 package noraui.data.console;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.junit.After;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import noraui.exception.TechnicalException;
 
 public class OutputConsoleDataProviderUT {
-
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
 
     @Test
     public void testConstructorIsPublic() throws Exception {
@@ -40,16 +30,49 @@ public class OutputConsoleDataProviderUT {
         outputConsoleDataProvider.prepare("hello");
         Assert.assertTrue(true);
 
+        final TestAppender appender = new TestAppender();
+        final Logger logger = Logger.getRootLogger();
+        logger.addAppender(appender);
         outputConsoleDataProvider.writeFailedResult(1, "UT Failed Message");
-        Assert.assertTrue(outContent.toString().endsWith("----- FAILED at line 1 >  UT Failed Message -----\n"));
+        List<LoggingEvent> log = appender.getLog();
+        Assert.assertEquals(log.get(0).getLevel(), Level.ERROR);
+        Assert.assertTrue(log.get(0).getMessage().toString().endsWith("----- FAILED at line 1 >  UT Failed Message -----"));
 
         outputConsoleDataProvider.writeSuccessResult(2);
-        Assert.assertTrue(outContent.toString().endsWith("----- SUCCESS at line 2 -----\n"));
+        log = appender.getLog();
+        Assert.assertEquals(log.get(1).getLevel(), Level.INFO);
+        Assert.assertTrue(log.get(1).getMessage().toString().endsWith("----- SUCCESS at line 2 -----"));
 
         outputConsoleDataProvider.writeWarningResult(3, "UT Warning Message");
-        Assert.assertTrue(outContent.toString().endsWith("----- WARNING at line 3 > UT Warning Message -----\n"));
+        log = appender.getLog();
+        Assert.assertEquals(log.get(2).getLevel(), Level.WARN);
+        Assert.assertTrue(log.get(2).getMessage().toString().endsWith("----- WARNING at line 3 > UT Warning Message -----"));
 
         outputConsoleDataProvider.writeDataResult("title", 4, "UT title");
-        Assert.assertTrue(outContent.toString().endsWith("----- DATA RESULT at line 4 > [title] = UT title -----\n"));
+        log = appender.getLog();
+        Assert.assertEquals(log.get(3).getLevel(), Level.INFO);
+        Assert.assertTrue(log.get(3).getMessage().toString().endsWith("----- DATA RESULT at line 4 > [title] = UT title -----"));
+    }
+}
+
+class TestAppender extends AppenderSkeleton {
+    private final List<LoggingEvent> log = new ArrayList<>();
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
+
+    @Override
+    protected void append(final LoggingEvent loggingEvent) {
+        log.add(loggingEvent);
+    }
+
+    @Override
+    public void close() {
+    }
+
+    public List<LoggingEvent> getLog() {
+        return new ArrayList<>(log);
     }
 }
