b72cf140185a71707645de8886397ebfd20b3da7
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2017-07-24 22:41:29 +0200

    Update blog.sql

--- src/test/resources/data/in/blog.sql
+++ src/test/resources/data/in/blog.sql
@@ -1,4 +1,4 @@
 (select t.Blog as "Blog", t.Titre as "Titre", t.Texte as "Texte", t.Auteur as "Auteur", t.Note as "Note", '3' as Résultat from blog t where t.Auteur = 'anonymous')
 UNION
-select t.Blog as "Blog", t.Titre as "Titre", t.Texte as "Texte", t.Auteur as "Auteur", t.Note as "Note", '' as Résultat from blog t where t.Auteur <> 'anonymous')
+(select t.Blog as "Blog", t.Titre as "Titre", t.Texte as "Texte", t.Auteur as "Auteur", t.Note as "Note", '' as Résultat from blog t where t.Auteur <> 'anonymous')
 ORDER BY "Texte"
