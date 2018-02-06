21ab43f3616265d2c212f7dd881142914786a93d
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2018-02-06 23:36:32 +0100

    Create UserNameService.java

---
+++ src/main/java/com/github/noraui/service/UserNameService.java
@@ -0,0 +1,33 @@
+/**
+ * NoraUi is licensed under the licence GNU AFFERO GENERAL PUBLIC LICENSE
+ * 
+ * @author Nicolas HALLOUIN
+ * @author Stéphane GRILLON
+ */
+package com.github.noraui.service;
+
+import org.openqa.selenium.support.ui.Select;
+
+public interface UserNameService {
+
+    /**
+     * @param name
+     * @param otherName
+     * @return
+     */
+    boolean comparingNames(String name, String otherName);
+
+    /**
+     * @param text
+     * @param dropDown
+     * @return
+     */
+    int findOptionByIgnoreCaseText(String text, Select dropDown);
+
+    /**
+     * @param name
+     * @return
+     */
+    String getNormalizeName(String name);
+
+}
