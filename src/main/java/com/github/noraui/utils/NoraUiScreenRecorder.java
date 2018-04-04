498d6cc837e813238dc607ec3130a735b39be681
sgrillon14 <sgrillon@hotmail.fr>
2018-04-04 23:10:44 +0200

    Use the built-in formatting to construct this argument.

--- src/main/java/com/github/noraui/utils/NoraUiScreenRecorder.java
+++ src/main/java/com/github/noraui/utils/NoraUiScreenRecorder.java
@@ -40,7 +40,7 @@ public class NoraUiScreenRecorder extends ScreenRecorder {
         if (!movieFolder.exists()) {
             movieFolder.mkdirs();
         } else if (!movieFolder.isDirectory()) {
-            logger.error("\"" + movieFolder + "\" is not a directory.");
+            logger.error("\"{}\" is not a directory.", movieFolder);
             throw new IOException("\"" + movieFolder + "\" is not a directory.");
         }
         return new File(movieFolder, name + "-" + new SimpleDateFormat("yyyy-MM-dd HH.mm.ss").format(new Date()) + "." + Registry.getInstance().getExtension(fileFormat));
