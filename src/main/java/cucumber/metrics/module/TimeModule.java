c7138d27af86170a9c323887cfe35a3c3a012833
sgrillon14 <sgrillon@hotmail.fr>
2018-04-11 21:37:27 +0200

    Merge cucumber-metrics in NoraUi project

---
+++ src/main/java/cucumber/metrics/module/TimeModule.java
@@ -0,0 +1,41 @@
+/**
+ * NoraUi is licensed under the license GNU AFFERO GENERAL PUBLIC LICENSE
+ * 
+ * @author Nicolas HALLOUIN
+ * @author Stéphane GRILLON
+ */
+package cucumber.metrics.module;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Binder;
+import com.google.inject.Inject;
+
+import cucumber.metrics.annotation.time.Time;
+import cucumber.metrics.annotation.time.Times;
+import cucumber.metrics.interceptor.TimeInterceptor;
+
+public class TimeModule extends AbstractMetricsModule {
+
+    /**
+     * Specific logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(TimeModule.class.getName());
+
+    public static final String TIME_ANNOTATION_ENABLE = "Time.annotation.enable";
+    public static final String TIMES_ANNOTATION_ENABLE = "Times.annotation.enable";
+
+    @Inject
+    TimeInterceptor timeInterceptor;
+
+    @Override
+    public void configure(Binder binder) {
+        logger.info("Cucumber Metrics Time configure");
+
+        // TimeInterceptor timeInterceptor = new TimeInterceptor();
+        setAnnotation2Interceptors(binder, TIME_ANNOTATION_ENABLE, Time.class, timeInterceptor);
+        setAnnotation2Interceptors(binder, TIMES_ANNOTATION_ENABLE, Times.class, timeInterceptor);
+    }
+
+}
