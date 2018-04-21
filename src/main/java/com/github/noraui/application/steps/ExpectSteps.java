545341c4ec167520cfff67b6a579aadea527023f
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2018-04-21 23:22:13 +0200

    fix import (JavascriptExecutor)

--- src/main/java/com/github/noraui/application/steps/ExpectSteps.java
+++ src/main/java/com/github/noraui/application/steps/ExpectSteps.java
@@ -11,6 +11,7 @@ import static com.github.noraui.utils.Constants.VALUE;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
