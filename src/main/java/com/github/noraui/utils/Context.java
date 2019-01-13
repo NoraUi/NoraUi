52a5afb71d7fe24b6e1ff13e860d0a2d33af8244
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-01-13 21:56:32 +0100

    java 8

--- src/main/java/com/github/noraui/utils/Context.java
+++ src/main/java/com/github/noraui/utils/Context.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Properties;
 
 import org.ini4j.Ini;
