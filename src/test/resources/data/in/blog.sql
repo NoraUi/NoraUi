ddd587f4be989cc6adaae9452c791b35f3e95b5b
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-03 09:38:36 +0200

    fix SQL blog (Postgres OK - try for MySQL)

--- src/test/resources/data/in/blog.sql
+++ src/test/resources/data/in/blog.sql
@@ -1,6 +1,6 @@
 select b.blog as "Blog", b.title as "Title", b.text as "Text", b.author as "Author", b.note as "Note", b."result" as "Result" from (
-  (select t.id, t.blog, t.title, t.text, t.author, t.note, '3' as result from blog t where t.author = 'anonymous')
+  (select t.id, t.blog, t.title, t.text, t.author, t.note, '3' as "result" from blog t where t.author = 'anonymous')
   UNION
-  (select t.id, t.blog, t.title, t.text, t.author, t.note, '' as result from blog t where t.author <> 'anonymous')
+  (select t.id, t.blog, t.title, t.text, t.author, t.note, '' as "result" from blog t where t.author <> 'anonymous')
   ORDER BY "id"
 ) b
