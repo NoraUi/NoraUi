91af409e66addeb70abfb80761074064054bb227
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-03 08:25:17 +0200

    fix sql blog

--- src/test/resources/data/in/blog.sql
+++ src/test/resources/data/in/blog.sql
@@ -1,4 +1,4 @@
-(select t.blog as "Blog", t.title as "Title", t.text as "Text", t.author as "Author", t.note as "Note", t.id as "id", '3' as Result from blog t where t.author = 'anonymous')
+select b."Blog", b."Title", b."Text", b."Author", b."Note" from ((select t.blog as "Blog", t.title as "Title", t.text as "Text", t.author as "Author", t.note as "Note", t.id as "id", '3' as Result from blog t where t.author = 'anonymous')
 UNION
 (select t.blog as "Blog", t.title as "Title", t.text as "Text", t.author as "Author", t.note as "Note", t.id as "id", '' as Result from blog t where t.author <> 'anonymous')
-ORDER BY "id"
+ORDER BY "id") b
