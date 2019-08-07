8ea29e54ac24dba8f3df9a9dd9d5ab899ccdb6f7
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-07 10:16:37 +0200

    fix Slf4j logger

--- src/main/java/cucumber/metrics/module/AbstractMetricsModule.java
+++ src/main/java/cucumber/metrics/module/AbstractMetricsModule.java
@@ -10,29 +10,29 @@ import static com.google.inject.matcher.Matchers.annotatedWith;
 import static com.google.inject.matcher.Matchers.any;
 
 import java.lang.annotation.Annotation;
-import java.util.logging.Logger;
 
 import org.aopalliance.intercept.MethodInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
 
-import cucumber.metrics.interceptor.TimeInterceptor;
 import cucumber.runtime.Env;
 
 public abstract class AbstractMetricsModule implements Module {
 
-    private static Logger logger = Logger.getLogger(TimeInterceptor.class.getName());
+    private final Logger LOGGER = LoggerFactory.getLogger(AbstractMetricsModule.class);
 
     void setAnnotation2Interceptors(Binder binder, String annotationEnable, final Class<? extends Annotation> annotationType, MethodInterceptor... interceptors) {
         String ae = Env.INSTANCE.get(annotationEnable);
         if ("false".equals(ae)) {
-            logger.info(annotationEnable + " set to false.");
+            LOGGER.info(annotationEnable + " set to false.");
         } else if ("true".equals(ae)) {
-            logger.info(annotationEnable + " set to true.");
+            LOGGER.info(annotationEnable + " set to true.");
             binder.bindInterceptor(any(), annotatedWith(annotationType), interceptors);
         } else {
-            logger.info(annotationEnable + " not set or wrong set (set to false by default).");
+            LOGGER.info(annotationEnable + " not set or wrong set (set to false by default).");
         }
     }
 
