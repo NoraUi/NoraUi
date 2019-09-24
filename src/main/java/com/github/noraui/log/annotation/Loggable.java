87dee0e39fa2da7e518bc4eefcf8dbf9dd4cd5c9
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-09-24 12:32:10 +0200

    fix javadoc

--- src/main/java/com/github/noraui/log/annotation/Loggable.java
+++ src/main/java/com/github/noraui/log/annotation/Loggable.java
@@ -1,3 +1,9 @@
+/**
+ * NoraUi is licensed under the license GNU AFFERO GENERAL PUBLIC LICENSE
+ * 
+ * @author Nicolas HALLOUIN
+ * @author Stéphane GRILLON
+ */
 package com.github.noraui.log.annotation;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -7,8 +13,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * @author Nicolas HALLOUIN & Stéohane GRILLON
- *         This annotation can be used on a class to inject a static Slf4J logger typed by the class of the declared logger.
+ * This annotation can be used on a class to inject a static Slf4J logger typed by the class of the declared logger.
  */
 @Target({ ElementType.TYPE })
 @Retention(RUNTIME)
