57321821690fee7d8e8970941c1f73cd135fb875
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-03-08 23:33:58 +0100

    Merge from Master

--- src/main/java/com/github/noraui/Constants.java
+++ src/main/java/com/github/noraui/Constants.java
@@ -15,6 +15,8 @@ public class Constants {
     public static final String DATE_FORMAT_REG_EXP = "(^(((0[1-9]|1[0-9]|2[0-8])[\\/](0[1-9]|1[012]))|((29|30|31)[\\/](0[13578]|1[02]))|((29|30)[\\/](0[4,6,9]|11)))[\\/](19|[2-9][0-9])\\d\\d$)|(^29[\\/]02[\\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)";
 
     public static final String VALUE = "value";
+    
+    public static final String PREFIX_SAVE = "SAVE ";
 
     public static final Charset DEFAULT_ENDODING = StandardCharsets.UTF_8;
 
