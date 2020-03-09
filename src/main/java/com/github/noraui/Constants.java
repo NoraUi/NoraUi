8218419229b2acdaa9b5fb8022d6695c9b17d0a3
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-03-09 20:14:40 +0100

    Merge from Master

--- src/main/java/com/github/noraui/Constants.java
+++ src/main/java/com/github/noraui/Constants.java
@@ -11,14 +11,38 @@ import java.nio.charset.StandardCharsets;
 
 public class Constants {
 
+    /**
+     * @deprecated As of release 4.1.4, replaced by {@link #FRENCH_DATE_FORMAT}
+     *             French date format.
+     */
+    @Deprecated
     public static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    /**
+     * @deprecated As of release 4.1.4, replaced by {@link #FRENCH_DATE_FORMAT_REG_EXP}
+     *             Regex to validate date format dd/mm/yyyy.
+     */
+    @Deprecated
     public static final String DATE_FORMAT_REG_EXP = "(^(((0[1-9]|1[0-9]|2[0-8])[\\/](0[1-9]|1[012]))|((29|30|31)[\\/](0[13578]|1[02]))|((29|30)[\\/](0[4,6,9]|11)))[\\/](19|[2-9][0-9])\\d\\d$)|(^29[\\/]02[\\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)";
 
+    /**
+     * French date format.
+     */
+    public static final String FRENCH_DATE_FORMAT = "dd/MM/yyyy";
+
+    /**
+     * Regex to validate date format dd/mm/yyyy.
+     */
+    public static final String FRENCH_DATE_FORMAT_REG_EXP = "(^(((0[1-9]|1[0-9]|2[0-8])[\\/](0[1-9]|1[012]))|((29|30|31)[\\/](0[13578]|1[02]))|((29|30)[\\/](0[4,6,9]|11)))[\\/](19|[2-9][0-9])\\d\\d$)|(^29[\\/]02[\\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)";
+
     public static final String VALUE = "value";
-    
+
     public static final String PREFIX_SAVE = "SAVE ";
 
     public static final Charset DEFAULT_ENDODING = StandardCharsets.UTF_8;
+
+    public static final String DEFAULT_ZONE_ID = "Europe/London";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
 
     public static final String USER_DIR = "user.dir";
     public static final String CLI_FILES_DIR = ".noraui";
