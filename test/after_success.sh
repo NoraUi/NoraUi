90893eefeaec48d94d651409e01f7a2019a7e250
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-05-04 13:59:25 +0200

    Automatically Publish Javadoc to GitHub

--- test/after_success.sh
+++ test/after_success.sh
@@ -5,6 +5,21 @@ echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
   		  
 if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/NoraUi' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    
+    echo "******** publish javadoc"
+    cd $HOME
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git clone --quiet --branch=master https://${GH_TOKEN}@github.com/NoraUi/NoraUi NoraUi > /dev/null
+
+    # Commit and Push the Changes
+    cd NoraUi
+    git rm -rf ./docs
+    cp -Rf $HOME/target/apidocs ./docs
+    git add -f .
+    git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to NoraUi"
+    git push -fq origin NoraUi > /dev/null
+    
     mvn_version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec)
     echo "Maven version is $mvn_version"
     if [[ $mvn_version == *"-SNAPSHOT" ]]; then
