a69bf50e771dd7273c45e2a9181e7b2cdf89c876
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-05-04 21:57:20 +0200

    Automatically Publish Javadoc to GitHub

--- test/after_success.sh
+++ test/after_success.sh
@@ -3,14 +3,14 @@
 echo "TRAVIS_REPO_SLUG is $TRAVIS_REPO_SLUG"
 echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
-echo "TRAVIS_COMMIT is $TRAVIS_COMMIT"
+echo "AUTHOR_NAME is $AUTHOR_NAME"
   		  
 if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/NoraUi' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     
     echo "******** publish javadoc"
     cd $HOME
     git config --global user.email "sgrillon@hotmail.fr"
-    git config --global user.name "Stéphane GRILLON"
+    git config --global user.name "Travis-CI"
     git clone --quiet --branch=master https://${GITHUB_TOKEN}@github.com/NoraUi/NoraUi NoraUi > /dev/null
 
     # Commit and Push the Changes
