0ac9082b772207900372f975465a17572be49a8a
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-05-04 16:37:08 +0200

    Automatically Publish Javadoc to GitHub

--- test/after_success.sh
+++ test/after_success.sh
@@ -7,6 +7,9 @@ echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/NoraUi' ] && [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     
     echo "******** publish javadoc"
+    cd $(dirname $0)
+    pwd
+    ls -l
     cd $HOME
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
@@ -15,8 +18,9 @@ if [ "$TRAVIS_REPO_SLUG" == 'NoraUi/NoraUi' ] && [ "$TRAVIS_BRANCH" == 'master' 
     # Commit and Push the Changes
     cd NoraUi
     git rm -rf ./docs
-    echo "******** $(dirname $0)"
-    cp -Rf `$(dirname $0)`/target/apidocs ./docs
+    pwd ls -l
+    
+    cp -Rf /target/apidocs ./docs
     git add -f .
     git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to NoraUi"
     git push -fq origin NoraUi > /dev/null
