5294d479e007ac54be0ac927a669da2cab9255ed
sgrillon14 <sgrillon@hotmail.fr>
2020-05-17 22:04:35 +0200

    change deprecated DATE_FORMAT_REG_EXP by DEFAULT_DATE_FORMAT_REG_EXP

--- src/main/java/com/github/noraui/Constants.java
+++ src/main/java/com/github/noraui/Constants.java
@@ -46,6 +46,7 @@ public class Constants {
 
     public static final String DEFAULT_ZONE_ID = "Europe/London";
     public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DEFAULT_DATE_FORMAT_REG_EXP = "(^(19|[2-9][0-9])\\d\\d[\\-](((0[1-9]|1[012]))[\\-](0[1-9]|1[0-9]|2[0-8])|((0[13578]|1[02]))[\\-](29|30|31)|((0[4,6,9]|11)[\\-](29|30)))$)|(^(19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)[\\-]02[\\-]29$)";
 
     public static final String USER_DIR = "user.dir";
     public static final String CLI_FILES_DIR = ".noraui";
