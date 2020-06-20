104b3fa7d510d48c1c11977a7c8b5bc749530233
Stéphane GRILLON <sgrillon@hotmail.fr>
2020-06-20 08:56:03 +0200

    add NoraUi License header

--- src/main/java/com/github/noraui/browser/waits/ChainableWait.java
+++ src/main/java/com/github/noraui/browser/waits/ChainableWait.java
@@ -1,3 +1,9 @@
+/**
+ * NoraUi is licensed under the license GNU AFFERO GENERAL PUBLIC LICENSE
+ *
+ * @author Nicolas HALLOUIN
+ * @author Stéphane GRILLON
+ */
 package com.github.noraui.browser.waits;
 
 import java.util.Collection;
